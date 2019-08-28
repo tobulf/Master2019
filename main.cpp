265a2af7383c32d709864a98290a05fb2b07c718
Tobias Rasmussen <tobulf@gmail.com>
2019-08-28 12:12:27 +0200

    Init new git repo

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Master-node.cpp
+ *
+ * Created: 28.08.2019 12:08:38
+ * Author : Tobias
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
