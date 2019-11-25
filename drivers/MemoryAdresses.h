5ac9d8f9c67956904526925dfa4197cd1f22e56f
Tobias Rasmussen <tobulf@gmail.com>
2019-11-25 18:07:27 +0100

    Update memory adresses

--- drivers/MemoryAdresses.h
+++ drivers/MemoryAdresses.h
@@ -8,8 +8,14 @@
 
 #ifndef MEMORYADRESSES_H_
 #define MEMORYADRESSES_H_
-
-
+/*EEPROM MEMORY ADRESSES, 0 - 1023 or 0x00 - 0x3FF */
+/* define start and end of eeprom */
+#define EEPROM_start 0x01
+#define EEPROM_end 0x02
+/* Define register names */
+#define EEPROM_is_initialized 0x00
+#define DEVICE_IS_JOINED 0x01
+#define MCU_REBOOTED 0x02
 
 
 
