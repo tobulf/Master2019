90913fc28ca913ec8ccca532e1a31578387902ae
Tobias Rasmussen <tobulf@gmail.com>
2019-12-04 20:35:23 +0100

    Add main for testing

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@
 #include <avr/io.h>
 #include <util/delay_basic.h>
 #include <stdbool.h>
+#include <avr/sleep.h>
+#include "drivers/power_management.h"
 #include "drivers/RN2483.h"
 #include "drivers/timer.h"
 #include "drivers/SPI_driver.h"
@@ -21,6 +23,8 @@
 #include "drivers/EEPROM.h"
 #include "drivers/i2cmaster.h"
 #include "drivers/mpu6050.h"
+#include "drivers/EEPROM.h"
+#include "drivers/MemoryAdresses.h"
 
 
 
@@ -39,37 +43,79 @@ extern "C" {
 #define test_bit(reg, bit ) (reg & (1 << bit))
 
 uint16_t threshold = 38;
+bool joined = false;
+
+#define CONVERTED_DATA
+
+adc AnalogIn;
+LED_driver Leds;
+timer stopwatch;
+RN2483 radio;
 
 int main (void){
-	USART_init();
-	adc AnalogIn;
-	LED_driver Leds;
+	sei();
 	Leds.toogle(RED);
-	timer stopwatch;
+	USART_init();
+	//EEPROM_init();
+	//joined = radio.init_OTAA(appEui,appKey);
+	//radio.set_DR(0);
+	//radio.set_duty_cycle(1, 0);
+	//radio.sleep();
 	mpu6050_init();
 	/* Enable interrupts */
-	sei();	
-	double a = 0;double b = 0;double c = 0;double d = 0;double e = 0;double f = 0;
-	stopwatch.set_time_out(1000);
+	//sei();
+	#ifdef CONVERTED_DATA	
+	double a = 0;double b = 0;double c = 0;double d = 0;double e = 0;double f = 0;double t;
+	#else
+	int16_t a = 0;int16_t b = 0;int16_t c = 0;int16_t d = 0;int16_t e = 0;int16_t f = 0;int16_t t;
+	#endif
+	stopwatch.set_time_out(50);
+	uint8_t time = 0;
 	uint16_t dataadc = 0;
-	mpu6050_init_interrupt();
-	uint8_t data = mpu6050_testConnection();
-	printf("%i \n", data);
-	AnalogIn.enable();
 	while (true){
-		AnalogIn.start_convertion(0);
-		dataadc = AnalogIn.read();
-		mpu6050_getConvData(&a, &b, &c, &d, &e, &f);
+		dataadc = AnalogIn.get_battery_lvl();
+		#ifdef CONVERTED_DATA
+		mpu6050_getConvAccData(&a, &b, &c);
+		mpu6050_getConvTempData(&t);
+		mpu6050_getConvGyroData(&d, &e, &f);
+		#else
+		mpu6050_getRawGyroData(&a, &b, &c);
+		mpu6050_getRawTempData(&t);
+		mpu6050_getRawAccData(&d, &e, &f);
+		#endif
 		if (stopwatch.time_out()){	
-			data = mpu6050_testConnection();
-			if(data){
-				printf("%s %i \n", "Adc:", dataadc);
+			//printf("Adc: %d \r\n",(dataadc));
+			time++;
+			uint8_t data = mpu6050_testConnection();
+			if(data && time==1){
+				time=0;
+				//printf("%s %i \n", "Adc:", dataadc);
+				#ifdef CONVERTED_DATA
+				mpu6050_getConvAccData(&a, &b, &c);
+				mpu6050_getConvTempData(&t);
+				mpu6050_getConvGyroData(&d, &e, &f);
 				_delay_ms(10);
+				int a2 = (int)(a + 0.5 - (a<0));
+				int b2 = (int)(b + 0.5 - (b<0));
+				int c2 = (int)(c + 0.5 - (c<0));
+				int t2 = (int)(t + 0.5 - (t<0));
 				int d2 = (int)(d + 0.5 - (d<0));
 				int e2 = (int)(e + 0.5 - (e<0));
 				int f2 = (int)(f + 0.5 - (f<0));
-				printf("%s %i %s %i %s %i \n","x: ", d2,"y: ", e2,"z: ", f2);
-				printf("%s %d \n", "ADC: ",dataadc);
+				//printf("ax: %i ay: %i az: %i temp: %i gx: %i gy: %i gz: %i \n", a2, b2, c2, t2, d2, e2, f2);
+				#else
+				mpu6050_getRawAccData(&a, &b, &c);
+				mpu6050_getRawTempData(&t);
+				mpu6050_getRawGyroData(&d, &e, &f);
+				_delay_ms(10);
+				printf("ax: %i ay: %i az: %i temp: %i gx: %i gy: %i gz: %i \n", a, b, c, t, d, e, f);
+				#endif
+				printf("Bat %d \n", dataadc);
+				//radio.wake();
+				Leds.toogle(GREEN);
+				//radio.TX_string(String(420), 1);
+				Leds.toogle(GREEN);
+				//radio.sleep();
 			}
 		}
 	}
@@ -78,7 +124,9 @@ int main (void){
 
 ISR(INT0_vect){
 	threshold++;
-	printf("Ext_int %u \r\n", threshold);
 	mpu6050_set_interrupt_thrshld(threshold);
-}
+};
+
+
+
 
