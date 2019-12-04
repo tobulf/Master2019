2f2e6ceb33f66aeefad5416e98aa31db3ae218b1
Tobias Rasmussen <tobulf@gmail.com>
2019-12-04 15:57:52 +0100

    Add custom AppEUI and AppKey

--- drivers/LoRa_cfg.h
+++ drivers/LoRa_cfg.h
@@ -10,8 +10,7 @@
 #define LORA_CFG_H_
 
 
-
-const char *appEui = "70B3D57ED0013824";
-const char *appKey = "93ADE996B818DE123617D64DF113BC4B";
+const char *appEui = "0004A30B0023A1EE";
+const char *appKey = "93ADE996B818DE123617D64DF113BC4A";
 
 #endif /* LORA_CFG_H_ */
