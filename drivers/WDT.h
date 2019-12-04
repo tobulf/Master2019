046aab3845d0c995100b935e94753fc9b65dba55
Tobias Rasmussen <tobulf@gmail.com>
2019-12-04 22:48:17 +0100

    Add new WDT with enums

--- drivers/WDT.h
+++ drivers/WDT.h
@@ -14,10 +14,23 @@
 
 #define WDT_reset() __asm__ __volatile__ ("wdr")
 
-void WDT_off(void);
-void WDT_set_prescaler();
+
+enum wdt_prescaler {WDT_16MS_PRESCALER= 0, 
+	WDT_32MS_PRESCALER,
+	WDT_64MS_PRESCALER, 
+	WDT_128MS_PRESCALER, 
+	WDT_025S_PRESCALER, 
+	WDT_05S_PRESCALER, 
+	WDT_1S_PRESCALER, 
+	WDT_2S_PRESCALER, 
+	WDT_4S_PRESCALER, 
+	WDT_8S_PRESCALER};
+
+void WDT_set_prescaler(wdt_prescaler prescaler);
+void WDT_stop();
 void WDT_INT_enable();
 void WDT_INT_RST_enable();
 void WDT_RST_enable();
+
 
 #endif /* WDT_H_ */
